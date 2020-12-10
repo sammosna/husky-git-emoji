af93e530c421765656a91f0ffb01413073e61eab
Jimmy <jimmy.beldone@gmail.com>
2020-12-10 18:36:28 +0100

    feat: semantic-release friendly

--- cli/commit.js
+++ cli/commit.js
@@ -6,16 +6,16 @@ let message = fs.readFileSync(process.env.HUSKY_GIT_PARAMS, "utf8");
 
 console.log("> husky-git-emoji parse")
 
-message = message.replace("revert:", "⏳");
-message = message.replace("build:", "📦");
-message = message.replace("ci:", "🤖");
-message = message.replace("docs:", "📖");
-message = message.replace("feat:", "🌟");
-message = message.replace("fix:", "🚑");
-message = message.replace("perf:", "⚡");
-message = message.replace("refactor:", "🚧");
-message = message.replace("style:", "💄");
-message = message.replace("test:", "✅");
-message = message.replace("tada:", "🎉");
+message = message.replace("revert:", "⏳  revert: ");
+message = message.replace("build:", "📦  build: ");
+message = message.replace("ci:", "🤖  ci: ");
+message = message.replace("docs:", "📖  docs: ");
+message = message.replace("feat:", "🌟  eat: ");
+message = message.replace("fix:", "🚑  fix: ");
+message = message.replace("perf:", "⚡  perf: ");
+message = message.replace("refactor:", "🚧  refactor: ");
+message = message.replace("style:", "💄  style: ");
+message = message.replace("test:", "✅  test: ");
+message = message.replace("tada:", "🎉  tada: ");
 
 fs.writeFileSync(process.env.HUSKY_GIT_PARAMS, message)
