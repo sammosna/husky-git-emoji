82160ec1df850c2df9b4f956fb2164a33b0ce7f7
Samuele Mosna <sam.mosna@gmail.com>
2022-09-22 12:32:32 +0200

    🌟  eat:  new husky approach

--- cli/init.js
+++ cli/init.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const child = require("child_process");
 const pm = require("../inc/pm")();
-
+const version = require("../package.json").version;
 
 console.log(`> using ${pm} to install dependencies`);
 
@@ -12,17 +12,20 @@ if (fs.existsSync(path.join(process.cwd(), "./package.json"))) {
     console.log("> package.json found!")
     console.log("> Installing husky")
     child.exec(`${pm} install husky --save-dev`, {}, () => {
-        console.log("> Add husky config ton package.json")
         let package_json = JSON.parse(fs.readFileSync(path.join(process.cwd(), "./package.json"), "utf8"));
-    
-        package_json.husky = {}
-        package_json.husky.hooks = {}
-        package_json.husky.hooks["commit-msg"] = "npx hge-commit";
-    
-        console.log("> Save package.json")
-        fs.writeFileSync(path.join(process.cwd(), "./package.json"), JSON.stringify(package_json, null, 4));
-    
-        console.log("> Installation OK!")
+        if (!package_json["husky-git-emoji-version"] || package_json["husky-git-emoji-version"] !== version) {
+            
+            child.execSync(`${pm} husky install`);
+            child.execSync(`${pm} husky add .husky/commit-msg "npx hge-commit"`);
+            
+            
+            
+            package_json["husky-git-emoji-version"] = version;
+            console.log("> Save package.json")
+            fs.writeFileSync(path.join(process.cwd(), "./package.json"), JSON.stringify(package_json, null, 4));
+
+            console.log("> Installation OK!")
+        }
     });
 } else {
     console.log("> package.json not found, please init your project with npm init");
